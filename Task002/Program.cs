685af0bc0c863ae4a596ef839e9349b6b6c1fbc7
Macbook pro <macbookpro@MacBook-Pro-Macbook.local>
2022-11-16 20:53:15 +0300

    add task004

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -1,5 +1,21 @@
-﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿// Напишите программу,
+// 1. на вход принимает два числа 
+// 2. выдаёт, какое число большее, а какое меньшее.
 
-// a = 5; b = 7 -> max = 7
-// a = 2 b = 10 -> max = 10
-// a = -9 b = -3 -> max = -3
+// // a = 5; b = 7 -> max = 7
+// // a = 2 b = 10 -> max = 10
+// // a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите первое чиcло : ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе чиcло : ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine("max = " + number1);
+}
+else
+{
+    Console.WriteLine("max = " + number2);
+}
