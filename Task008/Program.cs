8008b364bbe565e219df051ac5c1b98168320358
Macbook pro <macbookpro@MacBook-Pro-Macbook.local>
2022-11-16 22:01:31 +0300

    task008 finish

--- Task008/Program.cs
+++ Task008/Program.cs
@@ -4,3 +4,10 @@
 // 2. а на выходе показывает все чётные числа от 1 до N.
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= number; i++)
+{
+    if (i % 2 == 0) Console.Write("{0}, ", i);
+}
