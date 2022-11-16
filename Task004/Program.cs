b9e64f2ae51a6bd4bbad16f59ba594aea710faa3
Macbook pro <macbookpro@MacBook-Pro-Macbook.local>
2022-11-16 21:28:24 +0300

    add task006

--- Task004/Program.cs
+++ Task004/Program.cs
@@ -3,3 +3,16 @@
 // 2, 3, 7 -> 7
 // 44, 5, 78 -> 78
 // 22, 3, 9 -> 22
+
+Console.WriteLine("Введите первое число : ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число : ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число : ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if (number2 > number1) max = number2;
+if (number3 > max) max = number3;
+Console.WriteLine($"Максимальное число: {max}");
+
+
