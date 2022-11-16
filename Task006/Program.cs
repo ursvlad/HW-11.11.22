89f963f14f60fe024fbc9bb58b534ad53d3f82d1
Macbook pro <macbookpro@MacBook-Pro-Macbook.local>
2022-11-16 21:38:10 +0300

    finish prog

--- Task006/Program.cs
+++ Task006/Program.cs
@@ -4,3 +4,8 @@
 // // 4 -> да
 // -3 -> нет
 // 7 -> нет
+
+Console.WriteLine("Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
